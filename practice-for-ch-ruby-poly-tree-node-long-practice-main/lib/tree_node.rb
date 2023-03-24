cc94f9bb7b64164f2528ef2f0c79eaba8dafe07b
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-03-24 16:06:49 -0400

    first push

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,19 @@
+require "byebug"
 class PolyTreeNode
-  
+    attr_accessor :parent, :children, :value
+  def initialize(value)
+    @value = value 
+    @parent = nil 
+    @children = []  
+  end
+
+  def parent=(value)
+    if @parent != nil
+        parent.children = parent.children.select {|ele| ele != self}
+    end
+    @parent = value 
+    if !parent.children.include?(self)
+        value.children << self
+    end
+  end
 end
